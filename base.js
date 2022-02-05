37e1dd41bb84587ece5704ad24e2174fe279b659
rizveeerprojects <rarndc@gmail.com>
2022-02-05 14:06:37 +0600

    added responsive navigation

--- base.js
+++ base.js
@@ -25,6 +25,16 @@ $(document).ready(function(){
   }
   getCurrentUrl();
 
+  function detectDevice() {
+    let isMobile = false; //initiate as false
+    // device detection
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+        isMobile = true;
+    }
+    return isMobile
+  }
+
   function renderNavigation(){
     let string = '<ul class="navbar-nav ml-auto>';
     let active_status = "";
@@ -46,8 +56,45 @@ $(document).ready(function(){
     }
     string += '</ul>';
     $('#navbarNav').html(string);
+
+    // mobile navigation
+    string = "";
+    for(let i=0; i<options.keys.length; i++){
+      active_status = "";
+      if (current_key == options.keys[i]) {
+        active_status = "active";
+      }
+      if (active_status == "active") {
+        string += '<a style="color:#2e86c1;" href="'+options.pages[i]+'">'+options.names[i]+'</a>';
+      }
+      else{
+        string += '<a style="color:black;" href="'+options.pages[i]+'">'+options.names[i]+'</a>';
+      }
+    }
+    $('#myLinks').html(string);
+    /*let isMobile = detectDevice();
+    if (isMobile === true) {
+        $('#normal_navigation').hide();
+    }
+    else{
+        //$('#mobile_navigation').hide();
+    }*/
   }
   renderNavigation();
+
+  function screenSizeControl(x) {
+      if (x.matches) { // If media query matches
+        $('#normal_navigation').hide();
+        $('#mobile_navigation').show();
+      } else {
+        $('#normal_navigation').show();
+        $('#mobile_navigation').hide();
+      }
+  }
+
+  var x = window.matchMedia("(max-width: 900px)")
+  screenSizeControl(x) // Call listener function at run time
+  x.addListener(screenSizeControl) // Attach listener function on state changes
 
   function constructTable(column_names, column_values){
     let string = "";
@@ -106,5 +153,27 @@ $(document).ready(function(){
   });
 
   $('.dataTables_length').addClass('bs-select');
+
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  $('#logo_button').on('click', function(){
+    console.log("clicked");
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  });
+
+
+
 
 });
